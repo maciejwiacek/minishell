b3f20c4a528f439e1495060620fe221e16a1bc21
maciejwiacek <wiacem811_aehit@students.vizja.pl>
2024-08-25 01:01:33 +0200

    Redirects work if you expect it to work as not intended

--- exec/exec.c
+++ exec/exec.c
@@ -6,7 +6,7 @@
 /*   By: msmajdor <msmajdor@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/21 14:07:20 by msmajdor          #+#    #+#             */
-/*   Updated: 2024/08/24 23:16:51 by msmajdor         ###   ########.fr       */
+/*   Updated: 2024/08/25 00:59:55 by mwiacek          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,7 +30,7 @@ char *find_path(char *cmd)
 	return (NULL);
 }
 
-int	get_filein(t_cmd *cmd)
+int	get_outfile(t_cmd *cmd)
 {
 	int fd;
 	t_cmd	*tmp;
@@ -48,28 +48,123 @@ int	get_filein(t_cmd *cmd)
 		else
 			return (fd);
 	}
-	return (1);
+	return (-1);
 }
 
-void	exec(t_shell *shell)
+static int	get_infile(t_cmd *cmd)
 {
+	int		fd;
+	t_cmd	*tmp;
+
+	tmp = cmd;
+	while (tmp->op == LT)
+	{
+		fd = open(tmp->next->av[0], O_RDONLY);
+		if (fd == -1)
+		{
+			printf("minishell: %s: No such file or directory\n", tmp->next->av[0]);
+			return (-1);
+		}
+		tmp = tmp->next;
+		if (tmp->op == LT)
+			close(fd);
+		else
+			return (fd);
+	}
+	return (-1);
+}
+
+static void	handle_right(t_shell *shell, t_cmd *cmd)
+{
+	int		fd;
 	pid_t	pid;
-	int		filein;
 	char	*path;
 
 	pid = fork();
 	if (pid == 0)
 	{
-		filein = get_filein(shell->cmd);
-		if (filein != 1)
-			dup2(filein, 1);
-		if (mini_exec(shell, shell->cmd) == -1)
+		fd = get_outfile(shell->cmd);
+		if (fd != -1)
+			dup2(fd, STDOUT_FILENO);
+		else
+			exit(0);
+		if (mini_exec(shell, cmd) == -1)
 		{
-			path = find_path(shell->cmd->av[0]);
-			execve(path, shell->cmd->av, NULL);
+			path = find_path(cmd->av[0]);
+			execve(path, cmd->av, NULL);
 		}
 		else
 			exit(0);
 	}
 	waitpid(pid, NULL, 0);
+	close(fd);
+}
+
+static void	handle_left(t_shell *shell, t_cmd *cmd)
+{
+	int		fd;
+	int		fd2;
+	pid_t	pid;
+	char	*path;
+	t_shell *tmp;
+
+	pid = fork();
+	tmp = shell;
+	if (pid == 0)
+	{
+		fd = get_infile(shell->cmd);
+		if (fd != -1)
+			dup2(fd, STDIN_FILENO);
+		else
+			exit(0);
+		while (tmp->cmd->op == LT)
+			tmp->cmd = tmp->cmd->next;
+		if (tmp->cmd->op == GT || tmp->cmd->op == GTGT)
+			fd2 = get_outfile(tmp->cmd);
+		if (fd2 != -1)
+			dup2(fd2, STDOUT_FILENO);
+		else
+			exit(0);
+		if (mini_exec(shell, cmd) == -1) {
+			path = find_path(cmd->av[0]);
+			execve(path, cmd->av, NULL);
+		}
+		else
+			exit(0);
+	}
+	waitpid(pid, NULL, 0);
+	close(fd);
+}
+
+static void	handle_redir(t_shell *shell)
+{
+	char	*path;
+
+	if (shell->cmd->op == LT)
+	{
+		handle_left(shell, shell->cmd);
+		while (shell->cmd->op == LT)
+			shell->cmd = shell->cmd->next;
+		while (shell->cmd->op == GT || shell->cmd->op == GTGT)
+			shell->cmd = shell->cmd->next;
+	}
+	else if (shell->cmd->op == GT || shell->cmd->op == GTGT)
+	{
+		handle_right(shell, shell->cmd);
+		while (shell->cmd->op == GT || shell->cmd->op == GTGT)
+			shell->cmd = shell->cmd->next;
+	}
+	else
+	{
+		if (mini_exec(shell, cmd) == -1)
+		{
+			path = find_path(cmd->av[0]);
+			execve(path, cmd->av, NULL);
+		}
+	}
+}
+
+void	exec(t_shell *shell)
+{
+	handle_redir(shell);
 }
